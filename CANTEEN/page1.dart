104846e0da53b658e665c1c3581ccdeea3fdac43
Tilan Wiyandoowa <70626483+tilanprabudda@users.noreply.github.com>
2024-11-21 10:24:38 +0530

    Update page1.dart

--- CANTEEN/page1.dart
+++ CANTEEN/page1.dart
@@ -1,90 +1,154 @@
 import 'package:flutter/material.dart';
-import 'package:intl/intl.dart';
+import 'package:connectivity_plus/connectivity_plus.dart';
+import 'google_sheet_api_canteen.dart';
 
 class Page1 extends StatefulWidget {
+  const Page1({super.key});
+
   @override
-  _Page1State createState() => _Page1State();
+  Page1State createState() => Page1State();
 }
 
-class _Page1State extends State<Page1> {
-  DateTime? _selectedDate;
-  final TextEditingController _srinhoppesController = TextEditingController();
-  final TextEditingController _stringsController = TextEditingController();
-  final TextEditingController _lawariyaController = TextEditingController();
-  final TextEditingController _othersController = TextEditingController();
+class Page1State extends State<Page1> {
+  DateTime? _lastUpdateDate;
+  int stringCost = 0;
+  int lawariyaCost = 0;
+  int coconutCost = 0;
+  int totalCost = 0;
+  int totalProfit = 0;
+  int totalValue = 0;
+  DateTime? selectedDate;
+  String dayOfWeek = '';
+  int strhopval = 0;
+  int strhopvalall = 0;
+  int lawval = 0;
+  int otheval = 0;
 
-  int _totalValue = 0;
-  int _totalProft = 0;
-  int _totalCost = 0;
-  int _stringCost = 0;
-  int _lawariyaCost = 0;
-  int _coconutCost = 0;
-  int _strhopval = 0;
-  int _strhopvalall = 0;
-  int _lawval = 0;
-  int _otheval = 0;
+  bool _isConnected = true;
+  bool _isDataUpdated = false;
+  final GoogleSheetsApi googleSheetsApi = GoogleSheetsApi(
+    'API Key',
+    'credentials.json',
+  );
 
-  void _pickDate() async {
-    DateTime? pickedDate = await showDatePicker(
+  @override
+  void initState() {
+    super.initState();
+    _checkConnectivity();
+    _fetchPreviousBalance();
+  }
+
+  Future<void> _checkConnectivity() async {
+    var connectivityResult = await Connectivity().checkConnectivity();
+    setState(() {
+      _isConnected = connectivityResult != ConnectivityResult.none;
+    });
+  }
+
+  Future<void> _fetchPreviousBalance() async {
+    if (_isConnected) {
+      try {
+        final rows = await googleSheetsApi.getRows();
+        if (rows.isNotEmpty) {
+          final lastRow = rows.last;
+          setState(() {
+            strhopval = _getValueFromRow(lastRow, 0);
+            strhopvalall = _getValueFromRow(lastRow, 1);
+            lawval = _getValueFromRow(lastRow, 2);
+            otheval = _getValueFromRow(lastRow, 3);
+            totalCost = _getValueFromRow(lastRow, 7);
+            selectedDate = _getDateFromRow(lastRow, 9);
+            dayOfWeek = _getStringFromRow(lastRow, 10);
+            _lastUpdateDate = DateTime.now();
+            _isDataUpdated = true;
+          });
+        } else {
+          setState(() {
+            _isDataUpdated = false; 
+          });
+        }
+      } catch (e) {
+        _showErrorDialog('Error fetching data: $e');
+      }
+    } else {
+      _showConnectionError();
+    }
+  }
+
+  int _getValueFromRow(List<dynamic> row, int index) {
+    if (index < row.length) {
+      return int.tryParse(row[index]?.toString() ?? '0') ?? 0;
+    }
+    return 0;
+  }
+
+  String _getStringFromRow(List<dynamic> row, int index) {
+    if (index < row.length) {
+      return row[index]?.toString() ?? '';
+    }
+    return '';
+  }
+
+  DateTime? _getDateFromRow(List<dynamic> row, int index) {
+    if (index < row.length) {
+      return DateTime.tryParse(row[index]?.toString() ?? '');
+    }
+    return null;
+  }
+
+  void _showConnectionError() {
+    _showErrorDialog('No internet connection. Please check your network settings and try again.');
+  }
+
+  void _showErrorDialog(String message) {
+    showDialog(
       context: context,
-      initialDate: DateTime.now(),
-      firstDate: DateTime(2024),
-      lastDate: DateTime(2101),
+      builder: (context) => AlertDialog(
+        title: const Text('Error'),
+        content: Text(message),
+        actions: <Widget>[
+          TextButton(
+            onPressed: () {
+              Navigator.of(context).pop();
+            },
+            child: const Text('OK'),
+          ),
+        ],
+      ),
     );
-
-    if (pickedDate != null && pickedDate != _selectedDate) {
-      setState(() {
-        _selectedDate = pickedDate;
-      });
-    }
   }
 
-  String _getDayOfWeek() {
-    if (_selectedDate == null) {
-      return '';
-    }
-    return DateFormat('EEEE').format(_selectedDate!);
-  }
-
-  void _calculateCosts(int srinhoppesValue, int stringsValue, int lawariyaValue, int othersValue) {
-    _totalValue = srinhoppesValue * 50 + stringsValue * 5 + lawariyaValue * 50 + othersValue;
-    _coconutCost = (80 * (0.005 * ((10 * srinhoppesValue) + stringsValue) + (0.05 * lawariyaValue))).toInt();
-    _stringCost = (2.4 * ((10 * srinhoppesValue) + stringsValue)).toInt();
-    _lawariyaCost = (17 * lawariyaValue).toInt();
-    _totalCost = _stringCost + _lawariyaCost;
-    _totalProft = _totalValue - _totalCost;
-    _strhopval = srinhoppesValue;
-    _strhopvalall = stringsValue;
-    _lawval = lawariyaValue;
-    _otheval = othersValue;
+  void _calculateCosts(int strhopval, int strhopvalall, int lawval, int otheval) {
+    setState(() {
+      totalValue = strhopval * 50 + strhopvalall * 5 + lawval * 50 + otheval;
+      coconutCost = (80 * (0.005 * ((10 * strhopval) + lawval) + (0.05 * otheval))).toInt();
+      stringCost = (2.4 * ((10 * strhopval) + strhopvalall)).toInt();
+      lawariyaCost = (17 * lawval).toInt();
+      totalCost = stringCost + lawariyaCost;
+      totalProfit = totalValue - totalCost;
+    });
   }
 
   void _submit() {
-    final int srinhoppesValue = int.tryParse(_srinhoppesController.text) ?? 0;
-    final int stringsValue = int.tryParse(_stringsController.text) ?? 0;
-    final int lawariyaValue = int.tryParse(_lawariyaController.text) ?? 0;
-    final int othersValue = int.tryParse(_othersController.text) ?? 0;
-
-    setState(() {
-      _calculateCosts(srinhoppesValue, stringsValue, lawariyaValue, othersValue);
-    });
+    _calculateCosts(strhopval, strhopvalall, lawval, otheval);
 
     Navigator.pushNamed(
       context,
       '/page5',
       arguments: {
-        'strhopval': _strhopval,
-        'strhopvalall': _strhopvalall,
-        'lawval': _lawval,
-        'otheval': _otheval,
-        'stringCost': _stringCost,
-        'lawariyaCost': _lawariyaCost,
-        'coconutCost': _coconutCost,
-        'totalCost': _totalCost,
-        'totalProft': _totalProft,
-        'totalValue': _totalValue,
-        'selectedDate': _selectedDate,
-        'dayOfWeek': _getDayOfWeek(),
+        'strhopval': strhopval,
+        'strhopvalall': strhopvalall,
+        'lawval': lawval,
+        'otheval': otheval,
+        'stringCost': stringCost,
+        'lawariyaCost': lawariyaCost,
+        'coconutCost': coconutCost,
+        'totalCost': totalCost,
+        'totalProfit': totalProfit,
+        'totalValue': totalValue,
+        'selectedDate': selectedDate,
+        'dayOfWeek': dayOfWeek,
+        '_lastUpdateDate': _lastUpdateDate,
       },
     );
   }
@@ -92,89 +156,38 @@ class _Page1State extends State<Page1> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Distribute Page For Canteen'),
-      ),
-      body: Padding(
-        padding: const EdgeInsets.all(16.0),
+      body: Center(
         child: Column(
-          crossAxisAlignment: CrossAxisAlignment.stretch,
+          mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             ElevatedButton(
-              onPressed: _pickDate,
-              child: Text('Select a date', style: TextStyle(fontSize: 18)),
+              onPressed: _isDataUpdated ? _submit : null, 
               style: ElevatedButton.styleFrom(
-                foregroundColor: Colors.white, backgroundColor: Colors.blue,
-                padding: EdgeInsets.symmetric(vertical: 16),
+                foregroundColor: Colors.white,
+                backgroundColor: _isDataUpdated ? Colors.green : Colors.grey,
+                padding: const EdgeInsets.symmetric(vertical: 16, horizontal: 24),
                 shape: RoundedRectangleBorder(
                   borderRadius: BorderRadius.circular(10),
                 ),
               ),
-            ),
-            SizedBox(height: 16),
-            if (_selectedDate != null)
-              Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Text('Selected Date: ${DateFormat.yMMMd().format(_selectedDate!)}', style: TextStyle(fontSize: 16)),
-                  Text('Day of Week: ${_getDayOfWeek()}', style: TextStyle(fontSize: 16)),
-                  SizedBox(height: 16),
-                ],
-              ),
-            TextField(
-              controller: _srinhoppesController,
-              decoration: InputDecoration(
-                labelText: 'Sringhoppes Packets',
-                border: OutlineInputBorder(),
-              ),
-              keyboardType: TextInputType.number,
-              style: TextStyle(fontSize: 16),
-            ),
-            SizedBox(height: 16),
-            TextField(
-              controller: _stringsController,
-              decoration: InputDecoration(
-                labelText: 'Extra Sringhoppes Count',
-                border: OutlineInputBorder(),
-              ),
-              keyboardType: TextInputType.number,
-              style: TextStyle(fontSize: 16),
-            ),
-            SizedBox(height: 16),
-            TextField(
-              controller: _lawariyaController,
-              decoration: InputDecoration(
-                labelText: 'Lawariya',
-                border: OutlineInputBorder(),
-              ),
-              keyboardType: TextInputType.number,
-              style: TextStyle(fontSize: 16),
-            ),
-            SizedBox(height: 16),
-            TextField(
-              controller: _othersController,
-              decoration: InputDecoration(
-                labelText: 'Others',
-                border: OutlineInputBorder(),
-              ),
-              keyboardType: TextInputType.number,
-              style: TextStyle(fontSize: 16),
-            ),
-            SizedBox(height: 20),
-            ElevatedButton(
-              onPressed: _submit,
-              child: Text('Next', style: TextStyle(fontSize: 18)),
-              style: ElevatedButton.styleFrom(
-                foregroundColor: Colors.white, backgroundColor: Colors.green,
-                padding: EdgeInsets.symmetric(vertical: 16),
-                shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(10),
-                ),
+              child: Text(
+                _isDataUpdated
+                    ? 'Data Updated Successfully!'
+                    : 'Waiting to See Check the Last Update',
+                style: const TextStyle(fontSize: 18),
               ),
             ),
+            const SizedBox(height: 30),
+            if (!_isDataUpdated) ...[
+              Text(
+                'Error: Failed to update data. Please try again.',
+                style: TextStyle(color: Colors.red, fontSize: 16),
+              ),
+            ],
           ],
         ),
       ),
     );
   }
 }
+
